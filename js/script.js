348756ccf6992e67bda4cda36b12c0e848b22337
Irene Vios <35557223+irevios@users.noreply.github.com>
2019-04-22 23:33:21 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -63,11 +63,11 @@ function cambiarPorcentajes(planta){
     	}
     }
     $(".temp span").html(temperatura+"ºC");
-    $("#statcirculosvg").css("--porcentajetempe",temperatura);
+    $("#statcirculosvg").css("porcentajetempe",temperatura);
     $(".humaire span").html(humaire+"%");
-    $("#statcirculosvg").css("--porcentajehumai",humaire);
+    $("#statcirculosvg").css("porcentajehumai",humaire);
     $(".humagua span").html(humtierra+"%");
-    $("#statcirculosvg").css("--porcentajehumti",humtierra);
+    $("#statcirculosvg").css("porcentajehumti",humtierra);
     $(".luz span").html(luminosidad+"%");
-    $("#statcirculosvg").css("--porcentajelumi",luminosidad);
+    $("#statcirculosvg").css("porcentajelumi",luminosidad);
 }
