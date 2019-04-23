0e4e43672b17c6b64ec1e4508afb0cde66ffefb1
Irene Vios <35557223+irevios@users.noreply.github.com>
2019-04-23 12:34:02 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,125 +1,72 @@
-<?xml-stylesheet type="text/css" href="../css/style.css" ?>
-<svg
-   xmlns:dc="http://purl.org/dc/elements/1.1/"
-   xmlns:cc="http://creativecommons.org/ns#"
-   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-   xmlns:svg="http://www.w3.org/2000/svg"
-   xmlns="http://www.w3.org/2000/svg"
-   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-   width="233.69632mm"
-   height="233.69684mm"
-   viewBox="0 0 233.69632 233.69684"
-   version="1.1"
-   id="statcirculosvg"
-   inkscape:version="0.92.3 (2405546, 2018-03-11)"
-   sodipodi:docname="circular.svg">
-  <script xlink:href="../js/script.js" />
-  <script xlink:href="../js/jkl-parsexml.js"/>
-  <script xlink:href="https://code.jquery.com/jquery-3.2.1.slim.min.js"/>
-  <defs
-     id="defs2" />
-  <sodipodi:namedview
-     id="base"
-     pagecolor="#ffffff"
-     bordercolor="#666666"
-     borderopacity="1.0"
-     inkscape:pageopacity="0.0"
-     inkscape:pageshadow="2"
-     inkscape:zoom="0.55457546"
-     inkscape:cx="353.81762"
-     inkscape:cy="454.26322"
-     inkscape:document-units="mm"
-     inkscape:current-layer="g4559"
-     showgrid="false"
-     fit-margin-top="0"
-     fit-margin-left="0"
-     fit-margin-right="0"
-     fit-margin-bottom="0"
-     inkscape:window-width="1920"
-     inkscape:window-height="1057"
-     inkscape:window-x="-8"
-     inkscape:window-y="-8"
-     inkscape:window-maximized="1" />
-  <metadata
-     id="metadata5">
-    <rdf:RDF>
-      <cc:Work
-         rdf:about="">
-        <dc:format>image/svg+xml</dc:format>
-        <dc:type
-           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
-        <dc:title></dc:title>
-      </cc:Work>
-    </rdf:RDF>
-  </metadata>
-  <g
-     inkscape:label="Capa 1"
-     inkscape:groupmode="layer"
-     id="layer1"
-     transform="translate(396.55046,118.44962)">
-    <g
-       id="g4559">
-      <path
-         inkscape:connector-curvature="0"
-         id="circle825"
-         transform="scale(0.26458333)"
-         d="M -1057.1426,-447.68359 A 441.63143,441.63143 0 0 0 -1498.7734,-6.0507812 441.63143,441.63143 0 0 0 -1057.1426,435.58008 441.63143,441.63143 0 0 0 -615.51172,-6.0507812 441.63143,441.63143 0 0 0 -1057.1426,-447.68359 Z m 0,40.00976 c 221.79983,2.7e-4 401.62577,179.81934 401.62502,401.6191425 -3.4e-4,221.7990375 -179.82598,401.6247375 -401.62502,401.6249975 -221.799,-3e-4 -401.6189,-179.82598 -401.6191,-401.6249975 -8e-4,-221.7997925 179.8193,-401.6188025 401.6191,-401.6191425 z m 0,1.58594 c -220.9439,3.4e-4 -400.0339,179.08922 -400.0332,400.0332025 3e-4,220.9432275 179.09,400.0387175 400.0332,400.0390675 220.94332,-3.1e-4 400.04069,-179.09582 400.04103,-400.0390675 7.2e-4,-220.9439925 -179.09696,-400.0329025 -400.04103,-400.0332025 z m 0,35.20117 c 201.48388,2.5e-4 364.83862,163.34816 364.83791,364.8320325 C -692.3089,195.42849 -855.6633,358.78294 -1057.1465,358.7832 c -201.4831,-2.8e-4 -364.8318,-163.35472 -364.832,-364.8378875 -8e-4,-201.4838625 163.3482,-364.8317425 364.832,-364.8320325 z m 0,1.43945 c -200.7064,2.9e-4 -363.3933,162.68614 -363.3926,363.3925825 3e-4,200.7057575 162.6869,363.3981575 363.3926,363.3984375 200.70583,-2.7e-4 363.39815,-162.69266 363.39845,-363.3984375 6.8e-4,-200.7064525 -162.6919,-363.3923325 -363.39845,-363.3925825 z m -0.043,36.39844 c 180.5813,0 326.99419,146.41484 326.99419,326.9960956 0,180.5812544 -146.41289,326.9960944 -326.99419,326.9960944 -180.5812,0 -326.9882,-146.41484 -326.9882,-326.9960944 0,-180.5812556 146.407,-326.9960956 326.9882,-326.9960956 z m 0,2.00781 c -179.4972,0 -324.9804,145.4911 -324.9804,324.9882856 0,179.4971844 145.4832,324.9863244 324.9804,324.9863244 179.49722,0 324.98638,-145.48914 324.98638,-324.9863244 -10e-6,-179.4971856 -145.48916,-324.9882856 -324.98638,-324.9882856 z"
-         style="opacity:1;fill:#ffffff;fill-opacity:0.30578512;stroke:none;stroke-width:31.37649536;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal" />
-      <path
-         style="opacity:1;fill:none;fill-opacity:1;stroke:#000000;stroke-width:7;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal"
-         id="humai"
-         sodipodi:type="arc"
-         sodipodi:cx="-279.70236"
-         sodipodi:cy="-1.6011904"
-         sodipodi:rx="111.07189"
-         sodipodi:ry="111.07189"
-         sodipodi:start="3.1464425"
-         sodipodi:end="0.0033308384"
-         sodipodi:open="true"
-         d="m -390.77295,-2.13987 a 111.07189,111.07189 0 0 1 111.52491,-110.53228 111.07189,111.07189 0 0 1 110.61695,111.4409214" />
-      <path
-         id="tempe"
-         style="opacity:1;fill:none;fill-opacity:1;stroke:#000000;stroke-width:7;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal"
-         sodipodi:type="arc"
-         sodipodi:cx="-279.70236"
-         sodipodi:cy="-1.6011904"
-         sodipodi:rx="101.31761"
-         sodipodi:ry="101.31761"
-         sodipodi:start="3.1415927"
-         sodipodi:end="0.0089492894"
-         d="m -381.01997,-1.6011951 a 101.31761,101.31761 0 0 1 50.92078,-87.8943519 101.31761,101.31761 0 0 1 101.57823,0.454532 101.31761,101.31761 0 0 1 50.13215,88.34653308"
-         sodipodi:open="true" />
-      <path
-         style="opacity:1;fill:none;fill-opacity:1;stroke:#000000;stroke-width:7;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal"
-         id="humti"
-         sodipodi:type="arc"
-         sodipodi:cx="-279.70236"
-         sodipodi:cy="-1.6011904"
-         sodipodi:rx="91.613472"
-         sodipodi:ry="91.613472"
-         sodipodi:start="3.1415927"
-         sodipodi:end="0.0068358723"
-         d="m -371.31583,-1.6011947 a 91.613472,91.613472 0 0 1 45.98764,-79.4437613 91.613472,91.613472 0 0 1 91.79366,0.313748 91.613472,91.613472 0 0 1 45.4435,79.75627067"
-         sodipodi:open="true" />
-      <circle
-         r="74.923264"
-         cy="-1.6011904"
-         cx="-279.70236"
-         id="path815"
-         style="opacity:1;fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:0.32975274;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal" />
-      <path
-         id="lumi"
-         style="opacity:1;fill:none;fill-opacity:1;stroke:#000000;stroke-width:7;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:normal"
-         sodipodi:type="arc"
-         sodipodi:cx="-279.7139"
-         sodipodi:cy="-1.601601"
-         sodipodi:rx="81.257385"
-         sodipodi:ry="81.257385"
-         sodipodi:start="3.1415927"
-         sodipodi:end="0.011531758"
-         sodipodi:open="true"
-         d="m -360.97128,-1.6016048 a 81.257385,81.257385 0 0 1 40.89949,-70.5266102 81.257385,81.257385 0 0 1 81.52638,0.470078 81.257385,81.257385 0 0 1 40.08349,70.99355573" />
-    </g>
-  </g>
-</svg>
+// Fondo
+$(function fondo(){
+	var d = new Date();
+	var time = d.getHours()*3600 + d.getMinutes()*60 + d.getSeconds();
+	var giro = Math.round((time)*-1);
+	$('.hora').css('animation-delay',giro+'s');
+	$('.hora').css('-webkit-animation-delay',giro+'s');
+});
+
+
+// Menú circular
+$(function calculaCirculo(){
+	ancho = $('.menucircular')[0].getBoundingClientRect().height;
+	$('body').css('--circulo',ancho+'px');
+});
+
+$(window).resize(function calculaCirculo(){
+	var ancho = $('.menucircular')[0].getBoundingClientRect().height;
+	$('body').css('--circulo',ancho+'px');
+});
+
+function giramenu(grados,planta){
+	var deg = parseInt(grados);
+	var actual = parseInt($('body').css('--rotacion'));
+	if(actual == -270 && deg == 0){
+		$('body').css('--rotacion',(-1)*360+'deg');
+		setTimeout(function(){
+			$('.menucircular').css('transition','0s');
+			$('body').css('--rotacion',0+'deg')
+		},1000); 
+	}
+	else if(actual == 0 && deg == 270){
+		$('body').css('--rotacion',90+'deg');
+		setTimeout(function(){
+			$('.menucircular').css('transition','0s');
+			$('body').css('--rotacion',-270+'deg')
+		},1000); 
+	}
+	else{
+		$('body').css('--rotacion',(-1)*deg+'deg');
+	}
+
+	$('.menucircular').css('transition','1s');
+	cambiarPorcentajes(planta);
+}
+
+function cambiarPorcentajes(planta){
+	var archivo = "xml/datos.xml";
+    var xml = new JKL.ParseXML(archivo);
+    var datos = xml.parse();
+    var temperatura = 0;
+    var humaire = 0;
+    var humtierra = 0;
+    var luminosidad = 0;
+
+    for(var i=0;i<Object.keys(datos.APAM.registros.registro).length && temperatura == 0;i++){
+    	if(datos.APAM.registros.registro[i].planta === datos.APAM.plantas.planta[planta-1].id){
+    		temperatura = datos.APAM.registros.registro[i].temperatura;
+    		humaire = datos.APAM.registros.registro[i].humedad_aire;
+    		humtierra = datos.APAM.registros.registro[i].humedad_tierra;
+    		luminosidad = datos.APAM.registros.registro[i].luminosidad;
+    	}
+    }
+    $(".temp span").html(temperatura+"ºC");
+    $("#statcirculosvg").css("--porcentajetempe",parseInt(temperatura)+"");
+    $(".humaire span").html(humaire+"%");
+    $("#statcirculosvg").css("--porcentajehumai",parseInt(humaire)+"");
+    $(".humagua span").html(humtierra+"%");
+    $("#statcirculosvg").css("--porcentajehumti",parseInt(humtierra)+"");
+    $(".luz span").html(luminosidad+"%");
+    $("#statcirculosvg").css("--porcentajelumi",parseInt(luminosidad)+"");
+}
