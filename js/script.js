1c4ae85bb6261ea21fc39373e8ea7c6b1ba0e5e7
Irene Vios <35557223+irevios@users.noreply.github.com>
2019-05-09 14:20:52 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,3 +16,26 @@ $(window).resize(function calculaCirculo() {
     var ancho = $('.menucircular')[0].getBoundingClientRect().height;
     $('body').css('--circulo', ancho + 'px');
 });
+
+function giramenu(grados, planta) {
+    var deg = parseInt(grados);
+    var actual = parseInt($('body').css('--rotacion'));
+    if (actual == -270 && deg == 0) {
+        $('body').css('--rotacion', (-1) * 360 + 'deg');
+        setTimeout(function() {
+            $('.menucircular').css('transition', '0s');
+            $('body').css('--rotacion', 0 + 'deg');
+        }, 1000);
+    } else if (actual == 0 && deg == 270) {
+        $('body').css('--rotacion', 90 + 'deg');
+        setTimeout(function() {
+            $('.menucircular').css('transition', '0s');
+            $('body').css('--rotacion', -270 + 'deg');
+        }, 1000);
+    } else {
+        $('body').css('--rotacion', (-1) * deg + 'deg');
+    }
+    $('.menucircular').css('transition', '1s');
+    cambiarPorcentajes(planta);
+}
+
