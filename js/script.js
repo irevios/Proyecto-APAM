b207d46d0d8f8daf2039c742411b2c142e0ffa01
Irene Vios <35557223+irevios@users.noreply.github.com>
2019-05-09 14:19:53 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-
+// Fondo
+$(function fondo() {
+    var d = new Date();
+    var time = d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds();
+    var giro = Math.round((time) * -1);
+    $('.hora').css('animation-delay', giro + 's');
+    $('.hora').css('-webkit-animation-delay', giro + 's');
+    cambiarPorcentajes(1);
+});
+// Menú circular
+$(function calculaCirculo() {
+    var ancho = $('.menucircular')[0].getBoundingClientRect().height;
+    $('body').css('--circulo', ancho + 'px');
+});
+$(window).resize(function calculaCirculo() {
+    var ancho = $('.menucircular')[0].getBoundingClientRect().height;
+    $('body').css('--circulo', ancho + 'px');
+});
