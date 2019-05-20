d2bc0dddf733cd4cce11d114b7e294cc9b47d89d
Irene Vios <35557223+irevios@users.noreply.github.com>
2019-05-20 09:29:05 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,31 +83,31 @@ function cierraAdvertencias(){
 
 function compruebaAdvertencias(){
     var advertencias = "";
-    if($("#statcirculosvg").css("--porcentajetempe") <= "0"){
+    if($("#statcirculosvg").css("--porcentajetempe") <= 0){
         advertencias += "Temperatura demasiado baja. <br>";
     }
-    if($("#statcirculosvg").css("--porcentajetempe") >= "100"){
+    if($("#statcirculosvg").css("--porcentajetempe") >= 100){
         advertencias += "Temperatura demasiado alta. <br>";
     }
 
-    if($("#statcirculosvg").css("--porcentajehumai") <= "0"){
+    if($("#statcirculosvg").css("--porcentajehumai") <= 0){
         advertencias += "Insuficiente humedad en el aire. <br>";
     }
-    if($("#statcirculosvg").css("--porcentajehumai") >= "100"){
+    if($("#statcirculosvg").css("--porcentajehumai") >= 100){
         advertencias += "Demasiada humedad en el aire. <br>";
     }
 
-    if($("#statcirculosvg").css("--porcentajehumti") <= "0"){
+    if($("#statcirculosvg").css("--porcentajehumti") <= 0){
         advertencias += "Insuficiente humedad en la tierra. <br>";
     }
-    if($("#statcirculosvg").css("--porcentajehumti") >= "100"){
+    if($("#statcirculosvg").css("--porcentajehumti") >= 100){
         advertencias += "Demasiada humedad en la tierra. <br>";
     }
 
-    if($("#statcirculosvg").css("--porcentajelumi") <= "0"){
+    if($("#statcirculosvg").css("--porcentajelumi") <= 0){
         advertencias += "Necesita más luz solar. <br>";
     }
-    if($("#statcirculosvg").css("--porcentajelumi") >= "100"){
+    if($("#statcirculosvg").css("--porcentajelumi") >= 100){
         advertencias += "Demasiada luz solar. <br>";
     }
 
