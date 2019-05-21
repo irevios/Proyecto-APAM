49327de739fbbf36de4a2685e672baafdb082ac5
Irene Vios <35557223+irevios@users.noreply.github.com>
2019-05-21 13:19:22 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 // Ventana Responsive
 $(window).on('load', function cargando() {
-		$(".cargando").css("opacity","0");
-		setTimeout(function() { 
-			$(".cargando").hide();
-		}, 1000);
-		
+	$(".cargando").css("opacity", "0");
+	setTimeout(function() {
+		$(".cargando").hide();
+	}, 1000);
 });
 
 $(window).resize(function redimensionar() {
-	setTimeout(function() { calculaCirculo(); }, 150); 
+	setTimeout(function() {
+		calculaCirculo();
+	}, 150);
 });
 
 function calculaCirculo() {
@@ -24,112 +25,131 @@ $(function fondo() {
 	var desplazar = Math.round((time) * -1);
 	$('.hora').css('animation-delay', desplazar + 's');
 	$('.hora').css('-webkit-animation-delay', desplazar + 's');
-	if(desplazar > -21000 || desplazar < -75000){
+	if (desplazar > -21000 || desplazar < -75000) {
 		$('.planta').css("filter", "hue-rotate(31deg) saturate(0.8) brightness(0.7)");
 	}
 	cambiarPorcentajes(1);
 	calculaCirculo();
 });
 
-$(function nubes(){
+$(function nubes() {
 	generarNubes();
 	generarNubes();
 });
 
-function generarNubes(){
-	var num1 = Math.floor((Math.random() * 1250) + 100);
-	var num2 = Math.floor((Math.random() * 1250) + 100);
-	var parte1 = Math.floor((Math.random() * 10) + 1);
-	var parte2 = Math.floor((Math.random() * 4) + 1);
-	var lista = ["img/nube1.png","img/nube2.png","img/nube3.png", "img/nube4.png", "img/nube5.png"];
+function numAleatorio(minimo, maximo) {
+	return (Math.floor(Math.random() * (maximo - minimo + 1)) + minimo);
+}
+
+function generarNubes() {
+	var num1 = numAleatorio(1250, 100);
+	var num2 = numAleatorio(1250, 100);
+	var parte1 = numAleatorio(10, 1);
+	var parte2 = numAleatorio(4, 1);
+	var lista = ["img/nube1.png", "img/nube2.png", "img/nube3.png", "img/nube4.png", "img/nube5.png"];
 	var nube = lista[Math.floor(Math.random() * lista.length)];
 	var nube2 = lista[Math.floor(Math.random() * lista.length)];
-	if(nube2 == nube){
+	if (nube2 == nube) {
 		nube2 = lista[Math.floor(Math.random() * lista.length)];
 	}
-	$(".nubes").append('<img src='+nube+' class="nube" style="animation: nube '+num1+'s cubic-bezier(0.06, 0.35, 0.84, 0.32) infinite;animation-delay: '+-num1/parte1+'s;"/>');
-	$(".nubes").append('<img src='+nube2+' class="nube" style="animation: nube '+num2+'s cubic-bezier(0.43, 1.12, 0.85, 1) infinite;animation-delay: '+-num2/parte2+'s;"/>');
+	$(".nubes").append('<img src=' + nube + ' class="nube" style="animation: nube ' + num1 + 's cubic-bezier(0.06, 0.35, 0.84, 0.32) infinite;animation-delay: ' + -num1 / parte1 + 's;"/>');
+	$(".nubes").append('<img src=' + nube2 + ' class="nube" style="animation: nube ' + num2 + 's cubic-bezier(0.43, 1.12, 0.85, 1) infinite;animation-delay: ' + -num2 / parte2 + 's;"/>');
 }
 
 // Menú superior
-function abrirmenu(grados){
-	if($(".menuitem").hasClass('abierto')){
+function abrirMenu(grados) {
+	if ($(".menuitem").hasClass('abierto')) {
 		$(".menuitem").removeClass('abierto');
-	}
-	else{
-		$(".menuitem").addClass('abierto');   
+	} else {
+		$(".menuitem").addClass('abierto');
 	}
 }
 
-function muestraOptimos(){
-	if($(".optima").hasClass('mostrar')){
+function muestraOptimos() {
+	if ($(".optima").hasClass('mostrar')) {
 		$(".optima").removeClass('mostrar');
-	}
-	else{
-		$(".optima").addClass('mostrar');   
+	} else {
+		$(".optima").addClass('mostrar');
 	}
 }
 
 // Advertencias
 
-function cierraAdvertencias(){
-	if($(".advertencia").hasClass('cerrado')){
-		$(".advertencia").css("border-radius","0");
-		setTimeout(function(){
+function cierraAdvertencias() {
+	if ($(".advertencia").hasClass('cerrado')) {
+		$(".advertencia").css("border-radius", "0");
+		setTimeout(function() {
 			$(".advertencia").removeClass('cerrado');
-		} ,500);
-
-	}
-	else{
+		}, 500);
+	} else {
 		$(".advertencia").addClass('cerrado');
-		setTimeout(function(){
-			$(".advertencia").css("border-radius","50%");
-		} ,1000);
-
-	}   
+		setTimeout(function() {
+			$(".advertencia").css("border-radius", "50%");
+		}, 1000);
+	}
 }
 
-function compruebaAdvertencias(){
+function compruebaAdvertencias() {
 	var advertencias = "";
 	var valor = $("#statcirculosvg").css("--porcentajetempe");
-	if(parseInt(valor) <= 0){
+	if (parseInt(valor) <= 0) {
 		advertencias += "Temperatura demasiado baja. \n";
 	}
-	if(parseInt(valor) >= 100){
+	if (parseInt(valor) >= 100) {
 		advertencias += "Temperatura demasiado alta. \n";
 	}
 	valor = $("#statcirculosvg").css("--porcentajehumai");
-	if(parseInt(valor) <= 0){
+	if (parseInt(valor) <= 0) {
 		advertencias += "Insuficiente humedad en el aire. \n";
 	}
-	if(parseInt(valor) >= 100){
+	if (parseInt(valor) >= 100) {
 		advertencias += "Demasiada humedad en el aire. \n";
 	}
 	valor = $("#statcirculosvg").css("--porcentajehumti");
-	if(parseInt(valor) <= 0){
+	if (parseInt(valor) <= 0) {
 		advertencias += "Insuficiente humedad en la tierra. \n";
-	}   
-	if(parseInt(valor) >= 100){
+	}
+	if (parseInt(valor) >= 100) {
 		advertencias += "Demasiada humedad en la tierra. \n";
 	}
 	valor = $("#statcirculosvg").css("--porcentajelumi");
-	if(parseInt(valor) <= 0){
+	if (parseInt(valor) <= 0) {
 		advertencias += "Necesita más luz solar. \n";
 	}
-	if(parseInt(valor) >= 100){
+	if (parseInt(valor) >= 100) {
 		advertencias += "Demasiada luz solar. \n";
 	}
 
-	$(".advertencia .media-body").text(advertencias);        
-	if(advertencias != ""){
+	$(".advertencia .media-body").text(advertencias);
+	if (advertencias != "") {
 		$(".advertencia").show();
-	}
-	else{
-		$(".advertencia").hide();   
+	} else {
+		$(".advertencia").hide();
 	}
 }
+
+// Regar planta
+function regar() {
+	$('.regar').append('<div class="agua"></div>');
+	aguaCayendo();
+	setTimeout(function() {
+		$('.agua').remove();
+	}, 5000);
+}
+
+function aguaCayendo() {
+	for (i = 1; i < 150; i++) {
+		var margenIzq = numAleatorio(0, 500);
+		var margenDer = numAleatorio(-1000, 1400);
+		$('.agua').append('<div class="gota" id="gota' + i + '"></div>');
+		$('#gota' + i).css('left', margenIzq);
+		$('#gota' + i).css('top', margenDer);
+	}
+}
+
+
 // Menú plantas y estadisticas radial
-function giramenu(grados, planta) {
+function giraMenu(grados, planta) {
 	cambiarPorcentajes(planta);
 	cambiarImgPlanta(planta);
 	var deg = parseInt(grados);
@@ -142,7 +162,7 @@ function giramenu(grados, planta) {
 		}, 1000);
 	} else if (actual == 0 && deg == 270) {
 		$('body').css('--rotacion', 90 + 'deg');
-		setTimeout(function() { 
+		setTimeout(function() {
 			$('.menucircular').css('transition', '0s');
 			$('body').css('--rotacion', -270 + 'deg');
 		}, 1000);
@@ -152,25 +172,26 @@ function giramenu(grados, planta) {
 	$('.menucircular').css('transition', '1s');
 }
 
-function cambiarImgPlanta(planta){
+function cambiarImgPlanta(planta) {
 	var img = "";
-	if(planta == 1){
-		img= "img/gardenia.svg";
+	if (planta == 1) {
+		img = "img/gardenia.svg";
 		$('.planta > object').css('width', 'calc(var(--circulo)/1.5)');
 	}
-	if(planta == 2){
-		img= "img/anturio.svg";
+	if (planta == 2) {
+		img = "img/anturio.svg";
 		$('.planta > object').css('width', 'calc(var(--circulo)/1.25)');
 	}
-	if(planta == 3){
-		img= "img/cactus.svg";
+	if (planta == 3) {
+		img = "img/gardenia2.svg";
+		$('.planta > object').css('width', 'calc(var(--circulo)/1.5)');
+	}
+	if (planta == 4) {
+		img = "img/cactus.svg";
 		$('.planta > object').css('width', 'calc(var(--circulo)/1.1)');
 	}
-	if(planta == 4){
-		img= "";
-	}
 	$(".planta > object").attr("data", img);
-	
+
 }
 
 // Coger datos del XML
@@ -178,7 +199,7 @@ function cambiarPorcentajes(planta) {
 	$.ajax({
 		type: "GET",
 		//url: "xml/datos.xml",
-		url: "https://raw.githubusercontent.com/irevios/apam/master/xml/datos.xml", 
+		url: "https://raw.githubusercontent.com/irevios/apam/master/xml/datos.xml",
 		dataType: "xml",
 		success: function(xml) {
 			var id;
@@ -193,10 +214,10 @@ function cambiarPorcentajes(planta) {
 
 			$(xml).find("planta:eq(" + (planta - 1) + ")").each(function() {
 				id = $(this).attr("id");
-				tempopt= $(this).find("temperatura_opt");
-				haiopt= $(this).find("humedad_aire_opt");
-				htiopt= $(this).find("humedad_tierra_opt");
-				lumiopt= $(this).find("luminosidad_opt");
+				tempopt = $(this).find("temperatura_opt");
+				haiopt = $(this).find("humedad_aire_opt");
+				htiopt = $(this).find("humedad_tierra_opt");
+				lumiopt = $(this).find("luminosidad_opt");
 			});
 			for (var i = 0; i < $(xml).find("registro").length && temperatura === 0; i++) {
 				$(xml).find("registro:eq(" + i + ")").each(function() {
@@ -213,7 +234,7 @@ function cambiarPorcentajes(planta) {
 			$(".humaire.concreta span").html(humaire + "%");
 			$(".humagua.concreta span").html(humtierra + "%");
 			$(".luz.concreta span").html(luminosidad + "%");
-			
+
 			//Optimos
 			$(".temp.optima span").html(tempopt.attr("min") + "ºC / " + tempopt.attr("max") + "ºC");
 			$(".humaire.optima span").html(haiopt.attr("min") + "% / " + haiopt.attr("max") + "%");
@@ -221,23 +242,23 @@ function cambiarPorcentajes(planta) {
 			$(".luz.optima span").html(lumiopt.attr("min") + "% / " + lumiopt.attr("max") + "%");
 
 			// Circular
-			$("#statcirculosvg").css("--porcentajetempe", 0 +"");
+			$("#statcirculosvg").css("--porcentajetempe", 0 + "");
 			$("#statcirculosvg").css("--porcentajehumai", 0 + "");
 			$("#statcirculosvg").css("--porcentajehumti", 0 + "");
 			$("#statcirculosvg").css("--porcentajelumi", 0 + "");
 			setTimeout(function() {
-				var suma = (parseInt(temperatura) - parseInt(tempopt.attr("min"))) *100 /(parseInt(tempopt.attr("max")) - parseInt(tempopt.attr("min")));
-				$("#statcirculosvg").css("--porcentajetempe", suma+"");
-				suma = (parseInt(humaire) - parseInt(haiopt.attr("min"))) *100 /(parseInt(haiopt.attr("max")) - parseInt(haiopt.attr("min")));
+				var suma = (parseInt(temperatura) - parseInt(tempopt.attr("min"))) * 100 / (parseInt(tempopt.attr("max")) - parseInt(tempopt.attr("min")));
+				$("#statcirculosvg").css("--porcentajetempe", suma + "");
+				suma = (parseInt(humaire) - parseInt(haiopt.attr("min"))) * 100 / (parseInt(haiopt.attr("max")) - parseInt(haiopt.attr("min")));
 				$("#statcirculosvg").css("--porcentajehumai", suma + "");
-				suma = (parseInt(luminosidad) - parseInt(lumiopt.attr("min"))) *100 /(parseInt(lumiopt.attr("max")) - parseInt(lumiopt.attr("min")));
+				suma = (parseInt(luminosidad) - parseInt(lumiopt.attr("min"))) * 100 / (parseInt(lumiopt.attr("max")) - parseInt(lumiopt.attr("min")));
 				$("#statcirculosvg").css("--porcentajelumi", suma + "");
-				suma = (parseInt(humtierra) - parseInt(htiopt.attr("min"))) *100 /(parseInt(htiopt.attr("max")) - parseInt(htiopt.attr("min")));
+				suma = (parseInt(humtierra) - parseInt(htiopt.attr("min"))) * 100 / (parseInt(htiopt.attr("max")) - parseInt(htiopt.attr("min")));
 				$("#statcirculosvg").css("--porcentajehumti", suma + "");
 				compruebaAdvertencias();
 			}, 1000);
-			
-			
+
+
 		}
 	});
 }
